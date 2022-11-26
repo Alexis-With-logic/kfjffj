dabaa0161cf2af513c591e72522d7226913332e2
Alexis-With-logic <119010724+Alexis-With-logic@users.noreply.github.com>
2022-11-26 12:47:35 -0600

    Create unbanchat.js

---
+++ complementos/unbanchat.js
@@ -0,0 +1,27 @@
+let handler = async (m, { isOwner, text, isAdmin }) => { 
+ let who 
+ if (m.isGroup) { 
+ if (!(isAdmin || isOwner)) { 
+ global.dfail('admin', m, conn) 
+ throw false 
+ } 
+ if (isOwner) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat 
+ who = m.mentionedJid[0] ? m.mentionedJid[0] : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat 
+ } else { 
+ if (!isOwner) { 
+ global.dfail('owner', m, conn) 
+ throw false 
+ } 
+ who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat 
+ } 
+ try { 
+ if (who.endsWith('g.us')) global.db.data.chats[who].isBanned = false 
+ else global.db.data.users[who].banned = false 
+ m.reply(`*Uepaaa Chat desbaneado _alfinnn puedo hablar_*`) 
+ } catch (e) { 
+ throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙽𝙾 𝙴𝚂𝚃𝙰 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾 𝙴𝙽 𝙻𝙰 𝙱𝙰𝚂𝙴 𝙳𝙴 𝙳𝙰𝚃𝙾𝚂*` 
+ }} 
+ handler.help = ['unban'] 
+ handler.tags = ['owner', 'group'] 
+ handler.command = /^unban(chat)?$/i 
+ module.exports = handler
