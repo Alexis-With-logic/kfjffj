8ab647c5121665ab20c1268e4ccc827dd846e47e
Alexis-With-logic <119010724+Alexis-With-logic@users.noreply.github.com>
2022-11-26 10:28:18 -0600

    Create chatbot.js

---
+++ complementos/chatbot.js
@@ -0,0 +1,29 @@
+handler.all = async function (m) { 
+ let chat = global.db.data.chats[m.chat] 
+  
+ if (/^todo bien$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `𝑩𝒊𝒆𝒏 𝒄𝒂𝒑𝒐 😎 𝒚 𝒕𝒖`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^e$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `𝑸𝒖𝒆 𝒃𝒖𝒆𝒏𝒐 𝒔𝒂𝒃𝒆𝒓 𝒍𝒂 𝒍𝒆𝒕𝒓𝒂 𝒆`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^5492266466080|972529277406$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `[❗] 𝑵𝒐 𝒆𝒕𝒊𝒒𝒖𝒆𝒕𝒆𝒔  [❗] 𝑨 𝒎𝒊 𝒄𝒓𝒆𝒂𝒅𝒐𝒓 𝒔𝒊 𝒕𝒊𝒆𝒏𝒆 𝒂𝒍𝒈𝒖𝒏𝒂 𝒄𝒐𝒏𝒔𝒖𝒍𝒕𝒂 𝒐 𝒅𝒖𝒅𝒂, 𝒉𝒂𝒃𝒍𝒂𝒎𝒆 𝒆𝒍 𝒑𝒗 𝒔𝒐𝒍𝒐 𝒑𝒐𝒓 𝒕𝒆𝒎𝒂 𝒅𝒆 𝒃𝒐𝒕`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+  
+ } 
+  
+ if (/^¿que es un bot?|que es un bot$/i.test(m.text) ) { //sem prefixo 
+     conn.reply(m.chat, `*☆::¿𝐐𝐮𝐞́ 𝐞𝐬 𝐮𝐧 𝐁𝐨𝐭 𝐝𝐞 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩?::☆* 
+ - 𝐔𝐧 𝐁𝐨𝐭 𝐞𝐬 𝐮𝐧𝐚 𝐢𝐧𝐭𝐞𝐥𝐢𝐠𝐞𝐧𝐜𝐢𝐚 𝐚𝐫𝐭𝐢𝐟𝐢𝐜𝐢𝐚𝐥 𝐪𝐮𝐞 𝐫𝐞𝐚𝐥𝐢𝐳𝐚 𝐭𝐚𝐫𝐞𝐚𝐬 𝐪𝐮𝐞 𝐥𝐞 𝐢𝐧𝐝𝐢𝐪𝐮𝐞 𝐜𝐨𝐧 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬, 𝐞𝐧 𝐞𝐥 𝐜𝐚𝐬𝐨 𝐝𝐞 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐩𝐮𝐞𝐝𝐞𝐬 𝐜𝐫𝐞𝐚𝐫 𝐬𝐭𝐢𝐜𝐤𝐞𝐫𝐬, 𝐝𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐫 𝐦𝐮́𝐬𝐢𝐜𝐚, 𝐯𝐢𝐝𝐞𝐨𝐬, 𝐜𝐫𝐞𝐚𝐫 𝐥𝐨𝐠𝐨𝐬 𝐩𝐞𝐫𝐬𝐨𝐧𝐚𝐥𝐢𝐳𝐚𝐝𝐨𝐬 𝐲 𝐦𝐮𝐜𝐡𝐨 𝐦𝐚𝐬, 𝐞𝐬𝐭𝐨 𝐝𝐞 𝐟𝐨𝐫𝐦𝐚 𝐚𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐳𝐚𝐝𝐚, 𝐨 𝐬𝐞𝐚 𝐪𝐮𝐞 𝐮𝐧 𝐡𝐮𝐦𝐚𝐧𝐨 𝐧𝐨 𝐢𝐧𝐭𝐞𝐫𝐟𝐢𝐞𝐫𝐞 𝐞𝐧 𝐞𝐥 𝐩𝐫𝐨𝐜𝐞𝐬𝐨  
+ - 𝐏𝐚𝐫𝐚 𝐯𝐞𝐫 𝐞𝐥 𝐦𝐞𝐧𝐮́ 𝐝𝐞 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 𝐩𝐮𝐞𝐝𝐞𝐬 𝐮𝐬𝐚𝐫 #menu 
+ *「 𝙏͚͜͝͠𝙝͚͜͝͠𝙚͚͜͝͠ ͚͜͝͠Loli𝘽͚͜͝͠𝙤͚͜͝͠𝙩͚͜͝͠-MD͚͜͝͠  」`, m) //wm, null, [['Menu', '#menu']], m) 
+  
+ } 
+  
+     
+ return !0 } 
+ export default handler
