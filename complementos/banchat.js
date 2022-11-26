fed89616992bc2a600b48624fd9f8ca73247c624
Alexis-With-logic <119010724+Alexis-With-logic@users.noreply.github.com>
2022-11-26 11:07:43 -0600

    Create banchat.js

---
+++ complementos/banchat.js
@@ -0,0 +1,27 @@
+let handler = async (m, { conn, isOwner, text, isAdmin }) => { 
+ let who 
+ if (m.isGroup) { 
+ if (!(isAdmin || isOwner)) { 
+ global.dfail('admin', m, conn) 
+ throw false 
+ } 
+ if (isOwner) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat 
+ else who = m.chat 
+ } else { 
+ if (!isOwner) { 
+ global.dfail('owner', m, conn) 
+ throw false 
+ } 
+ who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat 
+ } 
+ try { 
+ if (who.endsWith('g.us')) global.db.data.chats[who].isBanned = true 
+ else global.db.data.users[who].banned = true 
+ m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙵𝚄𝙴 𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*\n\n*—◉ 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝚁𝙴𝙰𝙲𝙲𝙸𝙾𝙽𝙰𝚁𝙰 𝙰 𝙽𝙸𝙽𝙶𝚄𝙽 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙷𝙰𝚂𝚃𝙰 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝚁 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃*`) 
+ } catch (e) { 
+ throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙽𝙾 𝙴𝚂𝚃𝙰 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾 𝙴𝙽 𝙻𝙰 𝙱𝙰𝚂𝙴 𝙳𝙴 𝙳𝙰𝚃𝙾𝚂*` 
+ }} 
+ handler.help = ['ban'] 
+ handler.tags = ['owner', 'group'] 
+ handler.command = /^ban(chat)?$/i 
+ module.exports = handler
