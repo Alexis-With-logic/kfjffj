6360a6122bdc4eb69923e3228125ca9a75033bb7
Alexis-With-logic <119010724+Alexis-With-logic@users.noreply.github.com>
2022-11-26 11:08:48 -0600

    Create pack.js

---
+++ complementos/pack.js
@@ -0,0 +1,44 @@
+import db from '../lib/database.js' 
+ import fetch from 'node-fetch' 
+ let handler = async (m, { conn, command }) => { 
+ let url = pack[Math.floor(Math.random() * pack.length)] 
+ conn.sendButton(m.chat, `_🥵 Pack 🥵_`, author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)} 
+ handler.help = ['pack'] 
+ handler.tags = ['internet'] 
+ handler.command = /^(pack)$/i 
+ handler.register = true 
+ export default handler 
+  
+ global.pack = [ 
+   "https://i.imgur.com/XbW7FO2.jpg", 
+   "https://i.imgur.com/ciuzM98.jpg", 
+   "https://i.imgur.com/uHsrrrx.jpg", 
+   "https://i.imgur.com/PVi8YDi.jpg", 
+   "https://i.imgur.com/FLC3ZXE.jpg", 
+   "https://i.imgur.com/54m52tX.jpg", 
+   "https://i.imgur.com/OsxYPgQ.jpg", 
+   "https://i.imgur.com/vpw6Xnr.jpg", 
+   "https://i.imgur.com/aAyPrZx.jpg", 
+   "https://i.imgur.com/Gh3ORCd.jpg", 
+   "https://i.imgur.com/rjGhygM.jpg", 
+   "https://i.imgur.com/zdHVFEI.jpg", 
+   "https://i.imgur.com/kAplnSG.jpg", 
+   "https://i.imgur.com/15UiO8o.jpg", 
+   "https://i.imgur.com/qjjyr6G.jpg", 
+   "https://i.imgur.com/bQZRhBU.jpg", 
+   "https://i.imgur.com/vpw6Xnr.jpg", 
+   "https://i.imgur.com/aAyPrZx.jpg", 
+   "https://i.imgur.com/Gh3ORCd.jpg", 
+   "https://i.imgur.com/rjGhygM.jpg", 
+   "https://i.imgur.com/0MhmmF4.jpg", 
+   "https://i.imgur.com/2MX4wvq.jpg", 
+   "https://i.imgur.com/HYL5ggu.jpg", 
+   "https://i.imgur.com/7ZjOD2a.jpg", 
+   "https://i.imgur.com/zbEUy3m.jpg", 
+   "https://i.imgur.com/tZ6vlg6.jpg", 
+   "https://i.imgur.com/jdPns8O.jpg", 
+   "https://i.imgur.com/VyjBQHT.jpg", 
+   "https://i.imgur.com/ozAGqBD.jpg", 
+   "https://i.imgur.com/DsSj9S1.jpg", 
+   "https://i.imgur.com/KYHpjNc.jpg", 
+ ]
