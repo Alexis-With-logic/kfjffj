b0b42dde2f145610af2889033e48446e6ab0a3cb
Alexis-With-logic <119010724+Alexis-With-logic@users.noreply.github.com>
2022-11-26 11:13:39 -0600

    Create random_dog.js

---
+++ complementos/random_dog.js
@@ -0,0 +1,20 @@
+let fetch = require('node-fetch') 
+ let handler  = async (m, { conn, text }) => { 
+ try { 
+ let res = await fetch('https://random.dog/woof.json') 
+ let json = await res.json() 
+ if (json.status) throw json 
+ let caption = ` 
+ _Un perrito_ 🐕 
+ *Black_Bot* 
+ `.trim() 
+ conn.sendFile(m.chat, json.url, 'dog.jpg', caption, m) 
+ } catch (e) { 
+ console.log(e) 
+ throw '*[❗] 𝙴𝚁𝚁𝙾𝚁*' 
+ }} 
+ handler.help = ['dog'] 
+ handler.tags = ['general'] 
+ handler.command = /^dog$/i 
+ handler.fail = null 
+ module.exports = handler
