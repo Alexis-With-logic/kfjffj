082dda3e46c5500d1840761d43490bc2e6562c74
Alexis-With-logic <119010724+Alexis-With-logic@users.noreply.github.com>
2022-11-26 10:22:00 -0600

    Create attp_ttp.js

---
+++ complementos/attp_ttp.js
@@ -0,0 +1,32 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => { 
+ if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇𝚃𝙾*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*◉ ${usedPrefix + command} Mystic-Bot*` 
+ let teks = encodeURI(text) 
+  
+ if (command == 'attp') { 
+ let teksb = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text 
+ conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teksb }), 'sticker.webp', '', m, false, { asSticker: true })} 
+  
+ if (command == 'attp2') { 
+ conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })} 
+      
+ if (command == 'attp3') { 
+ conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp2?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })} 
+  
+ if (command == 'ttp5') { 
+ conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp6?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })} 
+      
+ if (command == 'ttp4') { 
+ conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp5?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })} 
+      
+ if (command == 'ttp3') { 
+ conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp3?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })} 
+      
+ if (command == 'ttp2') { 
+ conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp2?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })} 
+      
+ if (command == 'ttp') { 
+ conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })} 
+      
+ } 
+ handler.command = handler.help = ['ttp', 'ttp2', 'ttp3', 'ttp4', 'ttp5', 'attp', 'attp2', 'attp3'] 
+ module.exports = handler
