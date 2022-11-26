8b967b00e51d278ba4f50ce403fdb0bfaf5c70db
Alexis-With-logic <119010724+Alexis-With-logic@users.noreply.github.com>
2022-11-26 11:01:51 -0600

    Create meme.js

---
+++ complementos/meme.js
@@ -0,0 +1,34 @@
+let axios = require("axios") 
+ let handler = async (m, {command, conn}) => { 
+ let res = await axios(pickRandom(meme)) 
+ let json = res.data 
+ let url = json.url 
+ conn.sendButtonImg(m.chat, url, `_${command}_`.trim(), author, 'Mas Memes 🤣', `/${command}`, m)} 
+ handler.command = /^(meme)$/i 
+ module.exports = handler 
+  
+ function pickRandom(list) { 
+ return list[Math.floor(list.length * Math.random())]} 
+  
+ const meme = [ 
+ "https://meme-api.herokuapp.com/gimme/memesparaguay", 
+ "https://meme-api.herokuapp.com/gimme/mememexico", 
+ "https://meme-api.herokuapp.com/gimme/memeslatam", 
+ "https://meme-api.herokuapp.com/gimme/memeslatinoamerica", 
+ "https://meme-api.herokuapp.com/gimme/latammemes", 
+ "https://meme-api.herokuapp.com/gimme/memeslatinoamerica", 
+ "https://meme-api.herokuapp.com/gimme/latammemes", 
+ "https://meme-api.herokuapp.com/gimme/memesmexico", 
+ "https://meme-api.herokuapp.com/gimme/memeparaguay", 
+ "https://meme-api.herokuapp.com/gimme/memeslatam", 
+ "https://meme-api.herokuapp.com/gimme/memesmexico", 
+ "https://meme-api.herokuapp.com/gimme/mememexico", 
+ "https://meme-api.herokuapp.com/gimme/memeslatam", 
+ "https://meme-api.herokuapp.com/gimme/memeslatinoamerica", 
+ "https://meme-api.herokuapp.com/gimme/latammemes", 
+ "https://meme-api.herokuapp.com/gimme/memeslatinoamerica", 
+ "https://meme-api.herokuapp.com/gimme/latammemes", 
+ "https://meme-api.herokuapp.com/gimme/memesmexico", 
+ "https://meme-api.herokuapp.com/gimme/memeparaguay", 
+ "https://meme-api.herokuapp.com/gimme/memeslatam" 
+ ]
