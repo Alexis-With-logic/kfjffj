58a0ed6f375838ef1705b2de72c0a2984b5f1a0e
Alexis-With-logic <119010724+Alexis-With-logic@users.noreply.github.com>
2022-11-26 11:16:44 -0600

    Create Simi.js

---
+++ complementos/Simi.js
@@ -0,0 +1,11 @@
+let fetch = require('node-fetch') 
+ let handler = async (m, { text, usedPrefix, command }) => { 
+ if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇𝚃𝙾 𝙿𝙰𝚁𝙰 𝙷𝙰𝙱𝙻𝙰𝚁 𝙲𝙾𝙽 𝚂𝙸𝙼𝚂𝙸𝙼𝙸 𝙾 𝙴𝙻 𝙱𝙾𝚃*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} Hola Black_Bot*` 
+ let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, '')) 
+ let json = await res.json() 
+ if (json.success) m.reply(json.success) 
+ else throw json } 
+ handler.help = ['simsimi'] 
+ handler.tags = ['general'] 
+ handler.command = ['multi', 'simi', 'simsimi']  
+ module.exports = handler
